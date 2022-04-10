a511a79d19736a21c07630df2f0773d8a82338ba
ahammed <ahammedmd852@gmail.com>
2022-04-10 21:26:01 +0600

    useFirebase hook has been used

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Home = () => {
+    const { user } = useFirebase();
     return (
         <div>
             <h2>This Is Home</h2>
+            <p>Current User: {user ? user.displayName : "No user"}</p>
         </div>
     );
 };
